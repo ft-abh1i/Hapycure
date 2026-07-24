e985c3fe98771cf1c8e21692e37b598f50d7073b
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-07-24 09:50:30 +0530

    Bump menu cart cache

--- best-sw.js
+++ best-sw.js
@@ -1,4 +1,4 @@
-const PATCH_VERSION = '2026-07-24-category-menu-v8';
+const PATCH_VERSION = '2026-07-24-menu-cart-v9';
 const HTML_CACHE = 'hapycure-shell-' + PATCH_VERSION;
 
 const FIREBASE_CONFIG_SCRIPT = `
