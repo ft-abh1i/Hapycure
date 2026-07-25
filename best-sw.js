083175c2c684075234d487ad39754060a1f0ee13
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-07-25 07:22:19 +0530

    Balance homepage section spacing
    Add clearer separation between the order switch and category heading, and between the category grid and Recommended for you section.

--- best-sw.js
+++ best-sw.js
@@ -1,4 +1,4 @@
-const PATCH_VERSION = '2026-07-25-home-recommended-v16';
+const PATCH_VERSION = '2026-07-25-home-section-spacing-v17';
 const HTML_CACHE = 'hapycure-shell-' + PATCH_VERSION;
 
 const FIREBASE_CONFIG_SCRIPT = `
