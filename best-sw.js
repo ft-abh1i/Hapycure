fd63fcb1850f8fabc1d7a4430c8dbb80ee037b5e
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-09 22:44:10 +0530

    Invalidate hidden mess page cache

--- best-sw.js
+++ best-sw.js
@@ -1,4 +1,4 @@
-const PATCH_VERSION = '2026-08-09-live-mess-v40';
+const PATCH_VERSION = '2026-08-09-live-mess-v41';
 const HTML_CACHE = 'hapycure-shell-' + PATCH_VERSION;
 
 const NATIVE_SCROLL_PATCH = `
