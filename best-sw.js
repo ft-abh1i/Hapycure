b1079675cdd09fc9abdb84977693aa606ed67ce2
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-10 08:08:31 +0530

    Refresh optimized mess detail cache

--- best-sw.js
+++ best-sw.js
@@ -1,4 +1,4 @@
-const PATCH_VERSION = '2026-08-10-title-food-badge-v49';
+const PATCH_VERSION = '2026-08-10-detail-page-v50';
 const HTML_CACHE = 'hapycure-shell-' + PATCH_VERSION;
 
 const NATIVE_SCROLL_PATCH = `
