c0287a900408ecc76acb1b489f9508f36b1ff7dd
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-10 09:08:26 +0530

    Refresh admin-approved catalogue cache

--- best-sw.js
+++ best-sw.js
@@ -1,4 +1,4 @@
-const PATCH_VERSION = '2026-08-10-hidden-support-details-v55';
+const PATCH_VERSION = '2026-08-10-admin-approval-v56';
 const HTML_CACHE = 'hapycure-shell-' + PATCH_VERSION;
 
 const NATIVE_SCROLL_PATCH = `
