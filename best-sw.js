96669a0764dc56e94173378825249b8099725838
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-10 07:41:21 +0530

    Invalidate cached mess card details

--- best-sw.js
+++ best-sw.js
@@ -1,4 +1,4 @@
-const PATCH_VERSION = '2026-08-10-taller-card-image-v44';
+const PATCH_VERSION = '2026-08-10-clean-card-v45';
 const HTML_CACHE = 'hapycure-shell-' + PATCH_VERSION;
 
 const NATIVE_SCROLL_PATCH = `
