220d46ddc91f59b26fefb32160aad91b434a8d8c
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-10 07:58:56 +0530

    Refresh mess title badge cache

--- best-sw.js
+++ best-sw.js
@@ -1,4 +1,4 @@
-const PATCH_VERSION = '2026-08-10-revert-single-row-v48';
+const PATCH_VERSION = '2026-08-10-title-food-badge-v49';
 const HTML_CACHE = 'hapycure-shell-' + PATCH_VERSION;
 
 const NATIVE_SCROLL_PATCH = `
