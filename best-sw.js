53453443e2aff47fc49549ab8e342f0eaf11a2dc
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-10 08:31:40 +0530

    Refresh hidden support contact UI

--- best-sw.js
+++ best-sw.js
@@ -1,4 +1,4 @@
-const PATCH_VERSION = '2026-08-10-support-contacts-v54';
+const PATCH_VERSION = '2026-08-10-hidden-support-details-v55';
 const HTML_CACHE = 'hapycure-shell-' + PATCH_VERSION;
 
 const NATIVE_SCROLL_PATCH = `
