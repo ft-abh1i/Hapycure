86ad1f9cee448a3de22b6df9d5b306aa2fe3ba35
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-10 08:26:49 +0530

    Refresh support contact details

--- best-sw.js
+++ best-sw.js
@@ -1,4 +1,4 @@
-const PATCH_VERSION = '2026-08-10-action-colors-v53';
+const PATCH_VERSION = '2026-08-10-support-contacts-v54';
 const HTML_CACHE = 'hapycure-shell-' + PATCH_VERSION;
 
 const NATIVE_SCROLL_PATCH = `
