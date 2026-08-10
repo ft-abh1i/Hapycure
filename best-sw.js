f5ae0da9304ebcadd9a3329a4c9aac3699fa0690
Abhijeet shrivastav <abhijeetshrivastava132@gmail.com>
2026-08-10 08:21:41 +0530

    Refresh action color styles

--- best-sw.js
+++ best-sw.js
@@ -1,4 +1,4 @@
-const PATCH_VERSION = '2026-08-10-large-booking-v52';
+const PATCH_VERSION = '2026-08-10-action-colors-v53';
 const HTML_CACHE = 'hapycure-shell-' + PATCH_VERSION;
 
 const NATIVE_SCROLL_PATCH = `
